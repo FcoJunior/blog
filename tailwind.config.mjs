b1ef15989670c870048b634cda5bac72fafc39ed
Fco Junior <fcojunr@gmail.com>
2026-07-09 17:47:22 -0300

    feat: implement dark mode support and enhance UI styling across components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@ import { blue } from 'tailwindcss/colors';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
@@ -13,17 +14,57 @@ export default {
       colors: {
         grey: {
           900: '#141414',
+          800: '#242424',
           700: '#242424',
           600: '#292929',
+          500: '#4a4a4a',
           300: '#6F6F6F',
           200: '#898989',
+          150: '#e1e1dd',
           100: '#ebebeb',
-          50: '#FDFDFD'
+          75: '#f3f3f1',
+          50: '#FDFDFD',
+          25: '#f8f8f6'
         },
         primary: blue
       },
       typography: ({ theme }) => ({
         DEFAULT: {
+          css: {
+            '--tw-prose-body': theme('colors.grey.500'),
+            '--tw-prose-headings': theme('colors.grey.800'),
+            '--tw-prose-bold': theme('colors.grey.800'),
+            '--tw-prose-counters': theme('colors.primary.400'),
+            '--tw-prose-bullets': theme('colors.primary.400'),
+            '--tw-prose-code': theme('colors.primary.400'),
+            '--tw-prose-links': theme('colors.primary.400'),
+            '--tw-prose-quotes': theme('colors.grey.800'),
+            '--tw-prose-quote-borders': theme('colors.grey.150'),
+            '--tw-prose-hr': theme('colors.grey.150'),
+            code: {
+              backgroundColor: theme('colors.grey.75'),
+              padding: '4px',
+              borderRadius: '6px'
+            },
+            'code::before': { content: '""' },
+            'code::after': { content: '""' },
+            blockquote: {
+              backgroundColor: theme('colors.grey.75'),
+              color: theme('colors.grey.800'),
+              border: '0',
+              padding: '1em',
+              fontWeight: 'normal',
+              borderRadius: '6px',
+              fontStyle: 'normal'
+            },
+            'blockquote p:first-of-type::before': { content: '""' },
+            'blockquote p:last-of-type::after': { content: '""' },
+            a: {
+              textDecoration: 'none'
+            }
+          }
+        },
+        invert: {
           css: {
             '--tw-prose-body': theme('colors.grey.100'),
             '--tw-prose-headings': theme('colors.grey.100'),
@@ -36,25 +77,11 @@ export default {
             '--tw-prose-quote-borders': theme('colors.grey.600'),
             '--tw-prose-hr': theme('colors.grey.600'),
             code: {
-              backgroundColor: theme('colors.grey.700'),
-              padding: '4px',
-              borderRadius: '6px'
+              backgroundColor: theme('colors.grey.700')
             },
-            'code::before': { content: '""' },
-            'code::after': { content: '""' },
             blockquote: {
               backgroundColor: theme('colors.grey.600'),
-              color: theme('colors.grey.50'),
-              border: '0',
-              padding: '1em',
-              fontWeight: 'normal',
-              borderRadius: '6px',
-              fontStyle: 'normal'
-            },
-            'blockquote p:first-of-type::before': { content: '""' },
-            'blockquote p:last-of-type::after': { content: '""' },
-            a: {
-              textDecoration: 'none'
+              color: theme('colors.grey.50')
             }
           }
         }
